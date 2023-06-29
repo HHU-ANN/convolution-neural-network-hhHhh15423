a1d3ba069c73c28b398c3d3ba8343844e2aac649
hhHhh15423 <131933219+hhHhh15423@users.noreply.github.com>
2023-06-29 22:52:51 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,7 +1,5 @@
 # 在该文件NeuralNetwork类中定义你的模型 
 # 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
-
-
 import os
 
 os.system("sudo pip3 install torch")
@@ -13,32 +11,6 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-
-num_epochs = 25  # 50轮
-batch_size = 50  # 50步长
-learning_rate = 0.01  # 学习率0.01
-from torch.utils.data import DataLoader
-
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-device = torch.device('cpu')
-
-
-def read_data():
-    # 这里可自行修改数据预处理，batch大小也可自行调整
-    # 保持本地训练的数据读取和这里一致
-    transform = transforms.Compose([
-        transforms.Pad(4),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32),
-        transforms.ToTensor()])
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
-                                                 transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False,
-                                               transform=torchvision.transforms.ToTensor())
-    data_loader_train = DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=True)
-    data_loader_val = DataLoader(dataset=dataset_val, batch_size=batch_size, shuffle=False)
-    return dataset_train, dataset_val, data_loader_train, data_loader_val
-
 
 # 3x3 卷积定义
 def conv3x3(in_channels, out_channels, stride=1):
@@ -70,9 +42,8 @@ class ResidualBlock(nn.Module):
         out = self.relu(out)
         return out
 
-
+# ResNet定义
 class NeuralNetwork(nn.Module):
-    # ResNet定义
     def __init__(self, block, layers, num_classes=10):
         super(NeuralNetwork, self).__init__()
         self.in_channels = 16
@@ -111,10 +82,21 @@ class NeuralNetwork(nn.Module):
         return out
 
 
-model = NeuralNetwork(ResidualBlock, [2, 2, 2]).to(device)
+
+
+def read_data():
+    # 这里可自行修改数据预处理，batch大小也可自行调整
+    # 保持本地训练的数据读取和这里一致
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
+    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
+    data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
+    return dataset_train, dataset_val, data_loader_train, data_loader_val
+
 def main():
-    model = NeuralNetwork(ResidualBlock, [2, 2, 2]).to(device)  # 若有参数则传入参数
+    model = NeuralNetwork(ResidualBlock, [2, 2, 2]) # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
+    
